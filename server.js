f2a32b9d7ad788b8c4657d711e02baab5e52cf5d
Jonathan <jonathan-516@hotmail.com>
2024-02-13 19:08:05 -0300

    update

---
+++ server.js
@@ -0,0 +1,21 @@
+
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+const subscribers = require('./routes/subscribers')
+
+require('dotenv').config()
+
+app.use(express.json());
+
+mongoose.connect(process.env.DATABASE_STRING)
+const db = mongoose.connection
+db.on('error', (err)=> console.log(err))
+db.once('open', ()=> console.log('Databese Connected'))
+
+app.use('/subscribers', subscribers)
+
+const port = 3000;
+app.listen(port, () =>{
+    console.log(`Servidor escultando na porta http://localhost:${port}`)
+})
