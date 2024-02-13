62e0bf36418e42ecf9937a3ed6fc0ba6571f6d95
Jonathan <jonathan-516@hotmail.com>
2024-02-13 19:50:38 -0300

    update

--- server.js
+++ server.js
@@ -4,14 +4,21 @@ const app = express();
 const mongoose = require('mongoose');
 const subscribers = require('./routes/subscribers')
 
+const schedule = require('node-schedule');
+const data = new Date(2024, 1, 13, 19, 34);
+
+schedule.scheduleJob(data, () => {
+    mongoose.connect(process.env.DATABASE_STRING)
+    const db = mongoose.connection
+    db.on('error', (err)=> console.log(err))
+    db.once('open', ()=> console.log('Databese Connected'))
+})
+
 require('dotenv').config()
 
 app.use(express.json());
 
-mongoose.connect(process.env.DATABASE_STRING)
-const db = mongoose.connection
-db.on('error', (err)=> console.log(err))
-db.once('open', ()=> console.log('Databese Connected'))
+
 
 app.use('/subscribers', subscribers)
 
