5853c993de0861eff1b73f677634e76de1d6923c
Jonathan <jonathan-516@hotmail.com>
2024-02-17 20:29:32 -0300

    merge

--- server.js
+++ server.js
@@ -11,11 +11,12 @@ const iniciar = schedule.scheduleJob('*/30 * * * * *', function(){
     console.log('Database Connected - SEQUELIZE');
 });
 
+// const iniciar = schedule.scheduleJob('*/30 * * * * *', function(){
 // mongoose.connect(process.env.DATABASE_STRING);
 // const db = mongoose.connection;
-
 // db.on('error', (err) => console.log(err));
 // db.once('open', () => console.log('Database Connected'));
+// });
 
 app.use(express.json());
 app.use('/nodeCron', nodeCron);
