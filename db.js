6425cfacee8cf1281523f1e6f9cf9f3ee5e46b9e
Jonathan <jonathan-516@hotmail.com>
2024-02-22 13:44:34 -0300

    Separei a conexão com o banco de dados db.js

---
+++ db.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.DATABASE_STRING);
+    console.log('Database Connected');
+  } catch (error) {
+    console.error(error);
+    process.exit(1);
+  }
+};
+
+module.exports = connectDB;
--- server.js
+++ server.js
@@ -1,17 +1,12 @@
 const express = require('express');
 const app = express();
-const mongoose = require('mongoose');
+const connectDB = require('./db');
+
+connectDB();
+
 const books = require('./routes/books');
 const students = require('./routes/students');
 const adm = require('./routes/adm');
-require('dotenv').config();
-
-
-mongoose.connect(process.env.DATABASE_STRING);
-const db = mongoose.connection;
-db.on('error', (err) => console.log(err));
-db.once('open', () => console.log('Database Connected'));
-
 
 app.use(express.json());
 app.use('/', books);
